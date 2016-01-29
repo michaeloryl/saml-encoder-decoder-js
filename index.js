87c6897f496a5ca43113f7c64d1ebc1741e6fcec
Michael Oryl <michael@oryl.com>
2016-01-29 09:55:43 -0500

    Got everything working with Async and ZLIB

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var pako = require('pako');
 var zlib = require('zlib');
+var async = require('async');
 // Deflate 
 // 
 
@@ -7,26 +7,32 @@ var str = '<?xml version="1.0" encoding="UTF-8"?><samlp:AuthnRequest     xmlns:s
 
 console.log("ORIGINAL:\n", str);
 
-var forUrl;
-zEncodeForSaml(str, encodeCallback);
+async.waterfall([
+    async.apply(zEncodeForSaml, str),
+    encodeCallback,
+    zDecodeForSaml,
+    decodeCallback
+], function (err, result) {
+    // result now equals 'done'
+});
 
-function encodeCallback(err, encoded) {
-  if (err) {
-    console.log("ERROR ON ENCODE");
-    return;
-  }
+function encodeCallback(encoded, callback) {
   console.log("\n\nENCODED:\n", encoded);
-  zDecodeForSaml(encoded, decodeCallback);
+  callback(null, encoded);
 }
 
-function decodeCallback(err, decoded) {
-  if (err) {
-    console.log("ERROR ON DECODE");
-    return;
-  }
-  
+function decodeCallback(decoded, callback) {
   console.log("\n\nDECODED:\n", decoded);
+  callback(null, decoded);
 }
+
+/*
+var mySaml = "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz48c2FtbHA6QXV0aG5SZXF1ZXN0IAl4bWxuczpzYW1scD0idXJuOm9hc2lzOm5hbWVzOnRjOlNBTUw6Mi4wOnByb3RvY29sIglJRD0iZ29jYmlqam5wY2hkcGFpbGZjampoYWNmb2xma2NncGVwZ2htcGNkbyIgCVZlcnNpb249IjIuMCIJSXNzdWVJbnN0YW50PSIyMDE2LTAxLTI3VDE1OjU0OjI2WiIJUHJvdG9jb2xCaW5kaW5nPSJ1cm46b2FzaXM6bmFtZXMudGM6U0FNTDoyLjA6YmluZGluZ3M6SFRUUC1SZWRpcmVjdCIJUHJvdmlkZXJOYW1lPSJwZW5ubXV0dWFsLmNvbS9wcm9kcGxhY2UiCUFzc2VydGlvbkNvbnN1bWVyU2VydmljZVVSTD0iIi8%2B";
+
+zDecodeForSaml(mySaml, function(err, decoded) {
+    console.log("MySAML:", decoded);
+})
+*/
 
 //console.log("MY SAML:", decodeForSaml("fZJRa9swEMff9ymE3m1LipM4InbJ0mwrNCM0bqF9Gap8cQW2lOnksH37ObFbWgZ9lLj76X/30/LqT9uQE3g0zuaUx4wSsNpVxtY5vS+/RRm9Kr4sUbWNOMpVF17sHfzuAANZIYIPfd/aWexa8HvwJ6Ph/u42py8hHFEmCV+ImM+ymMWCswRBd96Ev1GrrKrBJ2eueuXokUPJdc83VoVLqDPqA4lPmJwwxpLG1cZScnOd018HJeaLSsNkqhfAqql6nsyyVFQiEweWqnlfhrv+KXOCnB5Ug3C+wQ5uLAZlQ04F42nE5hHPSj6VKZc8jfmcP1Gy8y447Zqvxg6L6byVTqFBaVULKIOW+9X2VoqYyeehCOWPstxFq36yg9LhAjmZCvzPviOn352rGyAbG8AfvUEg+3E3ZDvshpKHVy3irKUXZVEOIj5PcBzj0mLwJi9z+veEzwFvRmgxLr++xI21axN4i5zUqJIyjTaP23SzftyK9Wwm/nO8TN6HKMbjx79U/AM="));
 
@@ -36,11 +42,12 @@ function zEncodeForSaml(input, cb) {
       console.log('\n\nDeflated:\n', deflated);
       console.log("TypeOf deflated:", typeof deflated);
 
-      var b64 = new Buffer(deflated).toString('base64');
+      var b64 = deflated.toString('base64');
 
       console.log('\n\nBase64 Encoded:\n', b64);
       
-      return cb(null, encodeURIComponent(b64));
+      //return cb(null, encodeURIComponent(b64));
+      return cb(null, b64);
     } else {
       console.log("Error while deflating");
       return cb(err);
@@ -48,14 +55,15 @@ function zEncodeForSaml(input, cb) {
   });
 }
 
-function zDecodeForSaml(str, cb) {
-  var unB64 = new Buffer(decodeURIComponent(str), 'base64').toString('utf8')
+function zDecodeForSaml(encoded, cb) {
+  //var unB64 = new Buffer(decodeURIComponent(str), 'base64').toString('utf8')
+  var unB64 = new Buffer(encoded, 'base64')
   console.log('\n\nBase64 decoded:\n', unB64);
 
   zlib.unzip(unB64, function(err, inflated) {
     if (!err) {
       console.log('\n\nInflated:\n', inflated);
-      return cb(null, inflated);
+      return cb(null, inflated.toString('ascii'));
     } else {
       console.log("Error while inflating");
       return cb(err);
@@ -63,50 +71,4 @@ function zDecodeForSaml(str, cb) {
   });
 }
 
-function encodeForSaml(str) {
-  var input = stringToUA(str);
 
-  var deflated = pako.deflate(input, { to: 'string' });
-  
-  //console.log('\n\nDeflated:\n', deflated);
-
-  var b64 = new Buffer(deflated).toString('base64');
-
-  //console.log('\n\nBase64 Encoded:\n', b64);
-  
-  return encodeURIComponent(b64);
-}
-
-function decodeForSaml(str) {
-  var unB64 = new Buffer(decodeURIComponent(str), 'base64').toString('utf8')
-
-  //console.log('\n\nBase64 decoded:\n', unB64);
-
-  var compressed = stringToUA(unB64);
-  
-  var result = null;
-   
-  try {
-    result = pako.inflate(compressed, { to: 'string' });
-  } catch (err) {
-    console.log(err);
-  }
-  
-  return result;
-}
-
-function stringToUA(s) {
-  var ua = new Uint8Array(s.length);
-  for (var i = 0; i < s.length; i++) {
-    ua[i] = s.charCodeAt(i);
-  }
-  return ua;
-}
-
-function ua2text(ua) {
-  var s = '';
-  for (var i = 0; i < ua.length; i++) {
-    s += String.fromCharCode(ua[i]);
-  }
-  return s;
-}
