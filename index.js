ed69ac72818e899665b96b3d01823a48a55df31f
Michael Oryl <michael@oryl.com>
2016-01-29 16:18:12 -0500

    Converted into real module; demos broken out into separate file

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
 /* global Buffer */
 var zlib = require('zlib');
-var async = require('async');
-// Deflate 
-// 
 
-var str = '<?xml version="1.0" encoding="UTF-8"?><samlp:AuthnRequest     xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol"    ID="dghihjdeegajbpejllikgcpnkceilmnmblhaidgl"     Version="2.0"    IssueInstant="2016-01-27T19:57:05Z"    ProtocolBinding="urn:oasis:names.tc:SAML:2.0:bindings:HTTP-Redirect"    ProviderName="pennmutual.com/prodplace"    AssertionConsumerServiceURL=""/>';
+module.exports = {
+  encodeSamlRedirect: encodeSamlRedirect,
+  encodeSamlPost: encodeSamlPost,
+  decodeSamlRedirect: decodeSamlRedirect,
+  decodeSamlPost: decodeSamlPost
+};
 
-console.log("ORIGINAL:\n", str);
-
-async.waterfall([
-    async.apply(encodeForSaml, str),
-    encodeCallback,
-    decodeForSaml,
-    decodeCallback
-], function (err, result) {
-    // result now equals 'done'
-});
-
-function encodeCallback(encoded, callback) {
-  console.log("\n\nENCODED:\n", encoded);
-  callback(null, encoded);
-}
-
-function decodeCallback(decoded, callback) {
-  console.log("\n\nDECODED:\n", decoded);
-  callback(null, decoded);
-}
-
-function encodeForSaml(input, cb) {
-  zlib.deflateRaw(input, function(err, deflated) {
+function encodeSamlRedirect(input, cb) {
+  zlib.deflateRaw(input, function (err, deflated) {
     if (!err) {
       var b64 = deflated.toString('base64');
       return cb(null, encodeURIComponent(b64));
@@ -39,10 +20,10 @@ function encodeForSaml(input, cb) {
   });
 }
 
-function decodeForSaml(encoded, cb) {
+function decodeSamlRedirect(encoded, cb) {
   var deflated = new Buffer(decodeURIComponent(encoded), 'base64');
-  
-  zlib.inflateRaw(deflated, function(err, inflated) {
+
+  zlib.inflateRaw(deflated, function (err, inflated) {
     if (!err) {
       return cb(null, inflated.toString('ascii'));
     } else {
@@ -52,4 +33,10 @@ function decodeForSaml(encoded, cb) {
   });
 }
 
+function encodeSamlPost(input, cb) {
+  return cb(null, encodeURIComponent(new Buffer(input).toString('base64')));
+}
 
+function decodeSamlPost(encoded, cb) {
+  return cb(null, new Buffer(decodeURIComponent(encoded), 'base64').toString('ascii'));
+}
